2e24c8ffb5722ec07c3393a0f072493fd8e13048
andhikaprasetyo <73724762+andhikaprasetyo@users.noreply.github.com>
2021-04-06 15:42:12 +0700

    Add files via upload

---
+++ Basic/sum_variables.java
@@ -0,0 +1,10 @@
+package Basic;
+
+public class sum_variables {
+    public static void main(String[] args) {
+        int x = 7;
+        int y = 4;
+        int sum = x + y;
+        System.out.println(sum);
+    }
+}
