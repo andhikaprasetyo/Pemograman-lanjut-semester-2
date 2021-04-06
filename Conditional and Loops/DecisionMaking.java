bd9591f1dce40088cc10106eb5b0e4f49a5d209d
andhikaprasetyo <73724762+andhikaprasetyo@users.noreply.github.com>
2021-04-06 17:52:17 +0700

    Add files via upload

---
+++ Conditional and Loops/DecisionMaking.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class DecisionMaking {
+    public static void main(String[] args){
+        Scanner input = new Scanner(System.in);
+        System.out.print("nilai : ");
+        int nilai = input.nextInt();
+        if (nilai ==5 ) {
+            System.out.println("Yeah");
+        }
+    }
+}
---
+++ Conditional and Loops/IfElseStatements.java
@@ -0,0 +1,15 @@
+
+import java.util.Scanner;
+public class IfElseStatements {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.print("nilai : ");
+        int nilai = input.nextInt();
+
+        if(nilai >10) {
+            System.out.println("iya");
+        } else {
+            System.out.println("tidak");
+        }
+    }
+}
